8f4e5edbaaae636ac5b8d7454161858c81f70527
chenshunyi <chenshunyi@vip.house365.com>
2018-04-03 10:58:50 +0800

    陈顺意新建类

---
+++ test-1/test-1/CustomView.h
@@ -0,0 +1,13 @@
+//
+//  CustomView.h
+//  test-1
+//
+//  Created by chenshunyi on 2018/4/3.
+//  Copyright © 2018年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomView : UIView
+
+@end
---
+++ test-1/test-1/CustomView.m
@@ -0,0 +1,21 @@
+//
+//  CustomView.m
+//  test-1
+//
+//  Created by chenshunyi on 2018/4/3.
+//  Copyright © 2018年 mac. All rights reserved.
+//
+
+#import "CustomView.h"
+
+@implementation CustomView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
