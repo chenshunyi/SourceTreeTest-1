09f8c71f0e44a7f66b0c5825dee6751594d20028
dongyajie <1721929531@qq.com>
2018-04-03 10:26:46 +0800

    修改

---
+++ test-1/test-1/ViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  test-1
+//
+//  Created by mac on 18/4/3.
+//  Copyright © 2018年 mac. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    NSString*str=@"123";
+   
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
